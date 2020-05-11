4911cd5644872e8b04bd49ebbc934531e0fb0925
Amar Teragundi <amarteragundi@gmail.com>
2020-05-11 12:07:45 +0530

    adding compose with dev tools

--- src/index.js
+++ src/index.js
@@ -5,21 +5,23 @@ import "./index.css";
 import App from "./App";
 
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import reducer from "./reducer";
 import rootSaga from "./sagas";
+
+import { composeWithDevTools } from "redux-devtools-extension";
 
 /* eslint-disable no-underscore-dangle */
 const sagaMiddleware = createSagaMiddleware();
 
 const middleware = applyMiddleware(sagaMiddleware);
 
-const composeEnhancers =
-  (window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    window.__REDUX_DEVTOOLS_EXTENSION__()) ||
-  compose;
+// const composeEnhancers =
+//   (window.__REDUX_DEVTOOLS_EXTENSION__ &&
+//     window.__REDUX_DEVTOOLS_EXTENSION__()) ||
+//   compose;
 
-const store = createStore(reducer, composeEnhancers(middleware));
+const store = createStore(reducer, composeWithDevTools(middleware));
 
 /* eslint-enable */
 
